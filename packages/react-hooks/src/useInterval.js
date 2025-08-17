e6052c6eb83bac69855153aacd3efe20bd61707c
Jonathan Price <jon.price77@gmail.com>
2025-08-17 11:40:39 +1000

    Add useInterval hook example

---
+++ packages/react-hooks/src/useInterval.js
@@ -0,0 +1,17 @@
+// useInterval.js
+// Runs a callback at a specified interval
+import { useEffect, useRef } from 'react';
+
+export function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    if (delay === null) return;
+    const id = setInterval(() => savedCallback.current(), delay);
+    return () => clearInterval(id);
+  }, [delay]);
+}
